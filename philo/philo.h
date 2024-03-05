737726f92f7e0edc7d5e4e7ff161068f8e5aea8d
yusengok <ysengoku.tourisme@gmail.com>
2024-03-05 09:03:02 +0100

    Add makefile & philo.h

---
+++ philo/philo.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/05 08:37:30 by yusengok          #+#    #+#             */
+/*   Updated: 2024/03/05 08:57:30 by yusengok         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+/* ----- libraries ----- */
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <pthread.h>
+
+#endif
