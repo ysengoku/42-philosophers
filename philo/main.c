89adab2a675741684a659d9d9f9cd7d58b20f7da
ysengoku <ysengoku.tourisme@gmail.com>
2024-03-07 22:20:54 +0100

    Add main & utils

---
+++ philo/main.c
@@ -0,0 +1,20 @@
+
+#include "philo.h"
+
+int	main(int argc, char **argv)
+{
+	/*--- Check argc & argv ---*/
+	if (argc < 5 && argc > 6)
+	{
+		write(2, INVALID_ARGC, ft_strlen(INVALID_ARGC));
+		return (1);
+	}
+
+	/*--- Init data ---*/
+
+
+	/*--- Start therads ---*/
+
+
+	/*--- Clear & free ---*/
+}
---
+++ philo/utils.c
@@ -0,0 +1,11 @@
+#include "philo.h"
+
+size_t	ft_strlen(char *str)
+{
+	size_t	len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
