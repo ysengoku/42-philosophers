585b1aa055dc03a9a551e5adb58af57a8925c08a
ysengoku <ysengoku.tourisme@gmail.com>
2024-03-07 22:20:54 +0100

    Add main & utils

--- philo/philo.h
+++ philo/philo.h
@@ -21,4 +21,34 @@
 #include <sys/time.h>
 #include <pthread.h>
 
+/* ----- structures ----- */
+
+typedef struct s_fork
+{
+	int				id;
+	pthread_mutex_t	state;
+}				t_fork;
+
+typedef struct s_philo
+{
+	int		id;
+	t_fork	fork_r;
+	t_fork	fork_l;
+}				t_philo;
+
+typedef struct s_data
+{
+	int		philos_count;
+	long	time_to_eat;
+	long	time_to_die;
+	long	time_to_sleep;
+	s_philo	*philos;
+	s_fork	*forks;
+}				t_data;
+
+/* ----- error messages ----- */
+# define INVALID_ARGC "Invalid number of arguments\nUsage: ./philo (number_of_philosophers) (time_to_die) (time_to_eat) (time_to_sleep) (optional: number_of_times_each_philosopher_must_eat)\n"
+
+/* ----- utils ----- */
+size_t	ft_strlen(char *str);
 #endif
