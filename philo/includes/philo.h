06a8683469d3a7bad40cd4502e452cff3e50d421
yusengok <ysengoku.tourisme@gmail.com>
2024-03-11 14:46:50 +0100

    Add routine (on going)

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 08:37:30 by yusengok          #+#    #+#             */
-/*   Updated: 2024/03/11 11:46:59 by yusengok         ###   ########.fr       */
+/*   Updated: 2024/03/11 14:28:04 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,30 +25,34 @@
 # include <limits.h>
 
 /* --- structures ----------------------------------------------------------- */
-typedef struct s_fork
-{
-	int				id;
-	pthread_mutex_t	mutex;
-}				t_fork;
+// typedef struct s_fork
+// {
+// 	int				id;
+// 	pthread_mutex_t	mutex;
+// }				t_fork;
 
 typedef struct s_philo
 {
-	int			id;
-	t_fork		fork_r;
-	t_fork		fork_l;
-	suseconds_t	last_meal;
-	int			meals_eaten;
+	pthread_t		thread;
+	int				id;
+	// t_fork		fork_r;
+	// t_fork		fork_l;
+	pthread_mutex_t	fork_r;
+	pthread_mutex_t	fork_l;
+	long			last_meal_time;
+	int				meals_eaten;
 }				t_philo;
 
 typedef struct s_data
 {
-	int		philos_count; // int
-	int		time_to_die; // millisecond
-	int		time_to_eat; // millisecond : 1 millisecond == 1000 microseconds
-	int		time_to_sleep; // millisecond
-	int		meals_to_eat; // int
-	t_philo	*philos;
-	t_fork	*forks;
+	int				philos_count; // int
+	int				time_to_die; // millisecond
+	int				time_to_eat; // millisecond : 1 millisecond == 1000 microseconds
+	int				time_to_sleep; // millisecond
+	int				meals_to_eat; // int
+	t_philo			*philos;
+	// t_fork	*forks;
+	pthread_mutex_t	*forks;
 }				t_data;
 
 /* --- check & parsing ------------------------------------------------------ */
@@ -57,7 +61,10 @@ int		check_arg(char **argv, t_data *data);
 /* --- data init & clear ---------------------------------------------------- */
 int		init_forks(t_data *data);
 int		init_philos(t_data *data);
-int		destroy_forks(t_data *data);
+int		clear_forks(t_data *data);
+
+/* --- routine -------------------------------------------------------------- */
+void	*start_routine(t_data *data);
 
 /* --- utils ---------------------------------------------------------------- */
 size_t	ft_strlen(char *str);
