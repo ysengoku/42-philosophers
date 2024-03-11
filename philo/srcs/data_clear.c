e8bbdf42291bf0cbf56b4e5afea3623ac04ee60c
yusengok <yusengok@student.42.fr>
2024-03-11 14:58:28 +0100

    Update

--- philo/srcs/data_clear.c
+++ philo/srcs/data_clear.c
@@ -6,7 +6,7 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 11:44:38 by yusengok          #+#    #+#             */
-/*   Updated: 2024/03/11 11:48:38 by yusengok         ###   ########.fr       */
+/*   Updated: 2024/03/11 14:50:10 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,8 @@ int	clear_forks(t_data *data)
 	i = 0;
 	while (i < data->philos_count)
 	{
-		pthread_mutex_destroy(&(data->forks[i].mutex));
+		pthread_mutex_destroy(&(data->forks[i]));
 		i++;
 	}
-	free(data->forks);
 	return (0);
 }
