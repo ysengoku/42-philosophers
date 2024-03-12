e2badee398dad4f8e23abc7d40a208361f38dadc
yusengok <yusengok@student.42.fr>
2024-03-12 14:43:20 +0100

    Add death monitor (on going)

--- philo/srcs/monitor.c
+++ philo/srcs/monitor.c
@@ -1,41 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   data_clear.c                                       :+:      :+:    :+:   */
+/*   monitor.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/11 11:44:38 by yusengok          #+#    #+#             */
-/*   Updated: 2024/03/12 14:35:03 by yusengok         ###   ########.fr       */
+/*   Created: 2024/03/12 14:08:59 by yusengok          #+#    #+#             */
+/*   Updated: 2024/03/12 14:09:54 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int	clear_mutex(t_data *data)
-{
-	int	i;
-
-	i = 0;
-	while (i < data->philos_count)
-	{
-		pthread_mutex_destroy(&(data->forks[i]));
-		pthread_mutexattr_destroy(&(data->deaths[i]));
-		i++;
-	}
-	free(data->forks);
-	free(data->deaths);
-	return (0);
-}
-
-int	clear_philos(t_data *data)
-{
-	int	i;
-
-	i = 0;
-	while (i < data->philos_count)
-	{
-		// thread
-	}
-	free(data->philos);
-}
+// check all mutex death 
